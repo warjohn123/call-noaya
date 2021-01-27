a6c81a425f2fe2dfe1b2f9fa6103f681899a8b14
warjohn123 <warrencaruana1@gmail.com>
2021-01-27 17:17:19 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { Jutsu } from 'react-jutsu'
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [room, setRoom] = useState('')
+  const [name, setName] = useState('')
+  const [call, setCall] = useState(false)
+  const [password, setPassword] = useState('')
+
+  const handleClick = event => {
+    event.preventDefault()
+    if (room && name) setCall(true)
+  }
+
+  return call ? (
+
+    <div>
+      <Jutsu
+        roomName={room}
+        displayName={name}
+        containerStyles={{ width: '100vw', height: '100vh' }}
+        password={password}
+        onMeetingEnd={() => console.log('Meeting has ended')}
+        loadingComponent={<p>loading ...</p>}
+        errorComponent={<p>Oops, something went wrong</p>} />
     </div>
-  );
+  ) : (
+    <form>
+      <input id='room' type='text' placeholder='Room' value={room} onChange={(e) => setRoom(e.target.value)} />
+      <input id='name' type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
+      <input id='password' type='text' placeholder='Password (optional)' value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleClick} type='submit'>
+        Start / Join
+      </button>
+    </form>
+  )
 }
 
 export default App;
